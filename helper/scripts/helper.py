74085287352fb5d2f5cc18e38afea1b8c27eed3e
Faris Nafiah <nafiahfarisizzuddin@gmail.com>
2023-11-12 17:38:21 +0000

    Helper package to view tf with prevision

---
+++ helper/scripts/helper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import rospy
+import tf
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+
+if __name__ == '__main__':
+    rospy.init_node('tf_a')
+    listener = tf.TransformListener()
+
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        try:
+            (trans,rot) = listener.lookupTransform('/table_origin', '/base_link_test', rospy.Time(0))
+            (roll, pitch, yaw) = euler_from_quaternion (rot)
+            print(trans, rot)
+            print(trans, [roll, pitch, yaw])
+        except (tf.LookupException, tf.ConnectivityException):
+            continue
+
+    rate.sleep()
